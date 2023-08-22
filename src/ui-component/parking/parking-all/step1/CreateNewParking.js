96239769c2f5a2a9649769bb10bf25aaeef0aa83
linhdoanh <linhdase151281@gmail.com>
2023-08-22 22:43:00 +0700

    commit for see floors

--- src/ui-component/parking/parking-all/step1/CreateNewParking.js
+++ src/ui-component/parking/parking-all/step1/CreateNewParking.js
@@ -82,6 +82,8 @@ const CreateNewParking = () => {
     setFloors([...floors, newFloor]);
   };
 
+  console.log("floors", floors);
+
   const handleRemoveFloor = (index) => {
     setFloors(floors.filter((_, i) => i !== index));
   };
